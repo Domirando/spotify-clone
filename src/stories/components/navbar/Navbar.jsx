b230b57098fc21dfbf5d58f2fbe86416d9797c3d
Domirando <maisiedev@gmail.com>
2022-01-15 00:18:56 +0500

    adding navigator & icons

--- src/stories/components/navbar/Navbar.jsx
+++ src/stories/components/navbar/Navbar.jsx
@@ -3,17 +3,16 @@ import PropTypes from 'prop-types';
 import { IoChevronForwardOutline, IoChevronBackOutline } from "react-icons/io5";
 import { Button } from '../button/Button';
 import './navbar.css';
-import {IconContext} from "react-icons";
 
 export const Navbar = ({ user, onLogin, upgrade, onCreateAccount }) => (
   <nav>
     <div className="wrapper">
         <div className='navigator'>
           <span className="navigator-icon">
-            <IoChevronBackOutline size='30px' className='icon'/>
+            <IoChevronBackOutline size='28px' className='icon'/>
           </span>
           <span className="navigator-icon">
-            <IoChevronForwardOutline size='30px' className='icon'/>
+            <IoChevronForwardOutline size='28px' className='icon'/>
           </span>
         </div>
       <div className='w-4/12'>
