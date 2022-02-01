5ceea13673e3d15448ce56a4f04856a76835af63
Domirando <maisiedev@gmail.com>
2022-02-01 18:12:39 +0500

    adding components

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,13 @@
 import './Footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import {Grid, Slider} from "@material-ui/core";
+
 export default function Footer() {
     return (
         <div className='footer'>
@@ -6,12 +15,25 @@ export default function Footer() {
                 <h1>album</h1>
             </div>
             <div className="footer__center">
-                <h1>player controls</h1>
+                <ShuffleIcon className='footer__green'/>
+                <SkipPreviousIcon className='footer__green'/>
+                <PlayCircleOutlineIcon fontSize='large' className='footer__green'/>
+                <SkipNextIcon className='footer__icon'/>
+                <RepeatIcon className='footer__green'/>
             </div>
             <div className="footer__right">
-                <h1>volume controls</h1>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item={6}>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider/>
+                    </Grid>
+                </Grid>
             </div>
-
         </div>
     )
 }
