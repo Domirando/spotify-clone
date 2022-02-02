256e6ac20126bf24cd4199f7d9845cdcd89daff0
Domirando <maisiedev@gmail.com>
2022-02-02 16:29:14 +0500

    changed

--- src/reducer.js
+++ src/reducer.js
@@ -4,10 +4,8 @@ export const initialState = {
     playing: false,
     item: null,
     daily_mix: null
-    // token: 'BQBLDlP_C6p-mevz-9Ic3fncOEed7weI19C81UTEWgNPZsAlhYxEgLS8akkWSPHNtojuNSnbLNIcWQ8EhTdSix-T9DPpF9m2CltAe61Ts595gudY9HBHPJnEP2OPzuxX0QfQ_zU5x0LHjjuVWOy5EYMywIoPOqvMKwZxkLot7i6Lxr1L'
 }
 export const reducer = (state, action) => {
-
     switch (action.type){
         case 'SET_USER':
             return {
