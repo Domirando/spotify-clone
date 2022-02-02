16335a68eeb02d0be75a0dd160eae9872e78b106
Domirando <maisiedev@gmail.com>
2022-02-02 16:24:54 +0500

    styling

--- src/Footer.js
+++ src/Footer.js
@@ -20,11 +20,16 @@ export default function Footer() {
                 </div>
             </div>
             <div className="footer__center">
-                <ShuffleIcon className='footer__green'/>
-                <SkipPreviousIcon className='footer__icon'/>
-                <PlayCircleOutlineIcon fontSize='large' className='footer__icon'/>
-                <SkipNextIcon className='footer__icon'/>
-                <RepeatIcon className='footer__green '/>
+                {/*<div className="top-center-footer">*/}
+                    <ShuffleIcon className='footer__green'/>
+                    <SkipPreviousIcon className='footer__icon'/>
+                    <PlayCircleOutlineIcon fontSize='large' className='footer__icon'/>
+                    <SkipNextIcon className='footer__icon'/>
+                    <RepeatIcon className='footer__green '/>
+                {/*</div>*/}
+                {/*<div className="bottom-center-footer">*/}
+                {/*    <Slider />*/}
+                {/*</div>*/}
             </div>
             <div className="footer__right">
                 <Grid container spacing={2}>
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search";
 import {Avatar} from "@material-ui/core";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import {useDataLayerValue} from "./DataLayer";
 export default function Header(){
     const [{user}, dispatch] = useDataLayerValue()
     return(
         <div className='header'>
             <div className="header__left">
-                <SearchIcon/>
-                <input placeholder='Search for artist, song, ...' type='text' className='search__input'/>
+                <ChevronLeftIcon fontSize='large'  className='nav_active_nav_icon nav_icon'/>
+                <ChevronRightIcon fontSize='large'  className='nav_icon'/>
+                <div className="header_search">
+                    <SearchIcon/>
+                    <input placeholder='Search for artist, song, ...' type='text' className='search__input'/>
+                </div>
             </div>
             <div className="header__right">
                 <Avatar src={user?.images[0]?.url} alt={user.display_name}/>
